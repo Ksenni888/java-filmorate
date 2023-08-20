9eb6fe1d2503cb3e7361538f6b45e6cce244fe19
Ksenni888 <len4244@yandex.ru>
2023-08-20 13:24:16 +0300

    sprint 9 style

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -10,7 +10,7 @@ public class Film {
     private int id;
     @NotBlank (message = "Имя не может быть пустым")
     private String name;
-    @Size (max=200, message = "Описание должно быть не более 200 символов")
+    @Size (max = 200, message = "Описание должно быть не более 200 символов")
     private String description;
     private LocalDate releaseDate;
     @Positive (message = "Значение должно быть положительным")
