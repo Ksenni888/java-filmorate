d7f51b8386c5442ae3fbcab4782719bb2a5673c9
Ksenni888 <len4244@yandex.ru>
2023-08-20 13:18:46 +0300

    sprint 9 style

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequestMapping("/films")
 public class FilmController {
     private final HashMap<Integer, Film> films = new HashMap<>();
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final Logger log = LoggerFactory.getLogger(FilmController.class);
     private int unicId;
 
     @GetMapping
