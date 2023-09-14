b9bf1772605aa702494b0cb151660ee2e1c5138f
Ksenni888 <len4244@yandex.ru>
2023-09-14 12:47:07 +0300

    sprint 10 edits10

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
