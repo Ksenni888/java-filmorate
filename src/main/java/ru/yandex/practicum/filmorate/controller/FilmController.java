46fea429e69d472b09a8dee275281cbb5440cdca
Ksenni888 <len4244@yandex.ru>
2023-10-03 17:04:24 +0300

    sprint 11 style4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class FilmController {
     private static final Logger log = LoggerFactory.getLogger(FilmController.class);
     private final FilmService filmService;
+
     @Autowired
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
